300f71d25222587102ad1b116379df5f0bfe6649
mayeonn <mayeonn9@gmail.com>
2023-12-08 23:16:35 +0900

    Initial Commit

---
+++ DayTown/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DayTown
+//
+//  Created by 김하연 on 12/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DayTown/DayTownApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayTownApp.swift
+//  DayTown
+//
+//  Created by 김하연 on 12/8/23.
+//
+
+import SwiftUI
+
+@main
+struct DayTownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
